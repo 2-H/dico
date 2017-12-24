fca9b932f9df48f706aea562b7ba47be8ccff904
saleh <saleh.yms14@gmail.com>
2017-12-24 13:13:12 +0200

    generate constructors

--- Dico/src/dico/gui/classCreator/AttributeRow.java
+++ Dico/src/dico/gui/classCreator/AttributeRow.java
@@ -13,7 +13,7 @@ import javafx.beans.property.SimpleStringProperty;
  */
 public class AttributeRow {
 
-    private final SimpleStringProperty name;
+    private final SimpleStringProperty name;//بدنا نشوف شو هي
 
     private final SimpleStringProperty type;
 
