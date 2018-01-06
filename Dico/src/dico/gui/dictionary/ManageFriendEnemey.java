9edcb98b85b51de2ee9391be9945545bf990f03c
Ali Al-Jobouri <ali.al-jibouri_1997@outlook.com>
2018-01-06 16:08:10 +0200

    Finishing the Dictionary Class

--- Dico/src/dico/gui/dictionary/ManageFriendEnemey.java
+++ Dico/src/dico/gui/dictionary/ManageFriendEnemey.java
@@ -28,6 +28,7 @@ public class ManageFriendEnemey extends Application {
     }
 
     public static void main(String[] args) {
+        
         launch(args);
     }
 }
--- Dico/src/dico/gui/dictionary/ManageFriendEnemeyController.java
+++ Dico/src/dico/gui/dictionary/ManageFriendEnemeyController.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package dico.gui.dictionary;
+
+import dico.DictionaryFactory;
+import dico.models.Dictionary;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,40 +17,63 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+
 /**
  *
  * @author User
  */
 public class ManageFriendEnemeyController implements Initializable {
-    
-    
+
     @FXML
-    public void CreateDictionary()
-    {
-        
-    }
+    private Button btnAddToDictionary;
     @FXML
-    public void ChooseYourDictionary()
-    {
-        
-    }
-    
+    private Button btnSaveDictionary;
     @FXML
-    public void ChooseYourItem()
-    {
-        
-    }
-     @FXML
-    public void AddEnemy()
-    {
-        
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        //get all dictionary names from list of dictionaries
-       
-        }
-        
+    private ComboBox ComboBoxDic;
+    @FXML
+    private ComboBox ComboBoxIt;
+    @FXML
+    private ListView myFriends;
+    @FXML
+    private ListView myEnemies;
+
+    private static int counter = 0;
+
+    public ArrayList<String> Person = new ArrayList<>();
+    public ArrayList<String> Animal = new ArrayList<>();
+
+    @FXML
+    private void SaveDictionary() {
+        //Save all addding 
     }
 
+    @FXML
+    private void RemoveItem() {
+
+    }
+
+    @FXML
+    private void fillItems() {
+        String selectedDictionary = ComboBoxDic.getSelectionModel().getSelectedItem().toString();
+        Dictionary dic = DictionaryFactory.Instance.getDictionary(selectedDictionary);
+        ComboBoxIt.getItems().clear();
+        ComboBoxIt.getItems().addAll(dic.getKeySet());
+    }
+
+    public void editLists() {
+        String selectedDictionary = ComboBoxDic.getSelectionModel().getSelectedItem().toString();
+        Dictionary dic = DictionaryFactory.Instance.getDictionary(selectedDictionary);
+        Object o = ComboBoxIt.getItems().addAll(dic.getKeySet());
+        // List friends=dic.findFriends(o);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        for (String d : DictionaryFactory.Instance.getDictionaryNames()) {
+            ComboBoxDic.getItems().add(d);
+        }
+
+    }
+
+}
